aaa77f2a416e8490b717d0a146c4a2e07303def7
Patrick Phuong <patrickphuong@ymail.com>
2019-04-03 16:06:17 -0700

    Fixed MergeSort and QuickSort

--- MergeSort.java
+++ MergeSort.java
@@ -1,57 +1,46 @@
-
+import java.util.*;
 public class MergeSort {
-			
-	public static void main(String[] args) {
-		int[] A = {1, 0 , 7, 4, 9, 5};
-		
-		for(int i=0; i<A.length; i++) {
-			System.out.print(A[i] + ", ");
-		}
-		System.out.println("");
-		int[] S = Mergesort(A);
-		
-		for(int i=0; i<A.length; i++) {
-			System.out.print(S[i] + ", ");
-		}					
-	}
-			
+
 	public static int[] Mergesort(int[] Array) {
-		
-		int[] Lsort, Rsort; //Arrays to be sorted
-		int n = Array.length;
-		int r = (Array.length - 1);
-		int middle = r / 2; 
-		int Lsize = middle + 1;
-		int Rsize = n - Lsize;
-		int[] left = new int[Lsize];
-		int[] right = new int[Rsize];
-		
-		if( n == 1) {
+
+		if( Array.length == 1) {
 			return Array;
 		}
-		
+
+		int[] Lsorted, Rsorted; //Arrays to be sorted
+
+		int r = (Array.length - 1);
+		int l = 0;
+		int middle = l + (r-l)/2;
+
+		int Lsize = middle + 1;
+		int Rsize = r - middle;
+
+		int[] left = new int[Lsize]; // Need to split half of array to do recursion on each half
+		int[] right = new int[Rsize];
+
 		for(int i=0; i<Lsize; i++) {
 			left[i] = Array[i];
-		}		
-		
-		for(int j= 0; j< Rsize; j++){
-			right[j] = Array[Lsize + j];						
 		}
-		
-		Lsort = Mergesort(left);
-		Rsort = Mergesort(right);
-		
-		return Merge(Lsort, Rsort,Lsize, Rsize);
-		
+
+		for(int j= 0; j< Rsize; j++){
+			right[j] = Array[Lsize + j];
+		}
+
+		Lsorted = Mergesort(left); // Recursively call on each half
+		Rsorted = Mergesort(right);
+
+		return Merge(Lsorted, Rsorted,Lsize, Rsize);
+
 	}
-	
+
 	public static int[] Merge(int[] A, int[] B, int lsize, int rsize) {
-		
+
 		int i=0; //index in A
 		int j=0; //index in B
 		int k=0; //index in C
-		int[] C = new int[A.length + B.length];
-		
+		int[] C = new int[lsize + rsize];
+
 		while(i < lsize && j < rsize) {
 			if(A[i] < B[j]) {
 				C[k] = A[i];
@@ -60,20 +49,40 @@ public class MergeSort {
 				C[k] = B[j];
 				j++;
 			}
-			k++;		
+			k++;
 		}
-		
+
 		while(i < lsize) {
 			C[k] = A[i];
 			i++;
 			k++;
 		}
-		
+
 		while(j < rsize) {
 			C[k] = B[j];
 			j++;
 			k++;
 		}
 		return C;
+	}
+
+	public static void main(String[] args) {
+
+		int range = 40;
+		int min = 1;
+		int[] A = new int[11];
+		for(int i=0; i<11; i++){
+			A[i] = (int) ((Math.random() * range) + min);
+		}
+
+		for(int i=0; i<A.length; i++) {
+			System.out.print(A[i] + ", ");
+		}
+		System.out.println("");
+		int[] S = Mergesort(A);
+
+		for(int i=0; i<A.length; i++) {
+			System.out.print(S[i] + ", ");
+		}
 	}
 }
--- QuickSort.java
+++ QuickSort.java
@@ -1,74 +1,78 @@
 
 public class QuickSort {
-   public static void main(String[] args) {	   	   
-	   
-	   int[] A = {15, 3, 2, 1, 9, 5, 7, 8, 6};
-	   //int[] A = {5, 7, 3, 2, 4, 1};
-	   int start =  0;
-	   int end = A.length - 1;
-	   
-	   for(int i=0; i<A.length; i++) {
-		   System.out.print(A[i] + ", ");
-	   }
-	   System.out.println("");
-	   
-	   quicksort(A ,start, end);
-	   
-	   for(int i=0; i<A.length; i++) {
-		   System.out.print(A[i] + ", ");
-	   }
-   }
-   
-   public static void quicksort(int[] array, int start, int end) {
-	    
-	   if( start == end || start>end) {
-		   return;
-	   }	   	   
-	   int pivot = array[0];
-	   int head = 1;
-	   
-	   int index = partition(array, head, end, pivot);
-	   if(array[index] < array[0]) {
-		   swap(array, 0, index);
-		   
-	   } else {
-		   swap(array, 0, index-1);		   
-	   }
-	   
-	   quicksort(array, 0, index-1);
-	   quicksort(array, index + 1, end);
-   }
-   
-   public static int partition(int[] array, int head, int tail, int pivot) {
-	   while(head < tail) {   	
-		   
-		   while(array[head] < pivot && head < tail) {
-			   head++;			   			   
-		
-		   }
-		   
-		   
-		   
-		   while(array[tail] > pivot && head < tail) {
-               //if(head == tail) break;
-			   tail--;
-		   }   			 
-		   
-		   if(head < tail) {
-		   swap(array, head, tail);
-		   head++;
-		   tail--;
-		   
-		   }
-	   }
-	   return tail;
 
+   public static void quicksort(int[] array, int start, int end) {
+
+	   if( start >= end) {
+		   return;
+	   }
+	   int pivot = array[start];
+
+     int head = start+1;
+     int tail = end;
+
+	   int index = partition(array, head, tail, pivot);
+	   if(array[index] < array[start]) {
+		   swap(array, start, index);
+
+	   } else {
+		   swap(array, start, index-1);
+	   }
+
+	   quicksort(array, start, index-1);
+	   quicksort(array, index, end);
    }
-   
+
+   public static int partition(int[] array, int head, int tail, int pivot) {
+       while(head <= tail){
+  		   while(array[head] < pivot && head<tail) {
+  			   head++;
+  		   }
+
+  		   while(array[tail] > pivot && head != tail) {
+          //if(head == tail) break;
+  			   tail--;
+  		   }
+
+         if(head<tail){
+  		     swap(array, head, tail);
+         }else if(head == tail){
+           break;
+         }
+      }
+	   return tail;
+   }
+
    public static void swap(int[] array, int head, int tail) {
 	   int temp;
 	   temp = array[head];
 	   array[head] = array[tail];
 	   array[tail] = temp;
+   }
+
+   public static void main(String[] args) {
+
+    int range = 40;
+ 		int min = 1;
+ 		int[] A = new int[11];
+	   //int[] A = {15, 3, 2, 1, 9, 5, 7, 8, 6};
+     for(int i=0; i<11; i++){
+ 			A[i] = (int) ((Math.random() * range) + min);
+ 		}
+	   //int[] A = {5, 7, 3, 2, 4, 1};
+     //int[] A = {4,1,2,3};
+	   int start =  0;
+	   int end = A.length - 1;
+
+	   for(int i=0; i<A.length; i++) {
+		   System.out.print(A[i] + ", ");
+	   }
+	   System.out.println("");
+
+	   quicksort(A ,start, end);
+
+	   for(int i=0; i<A.length; i++) {
+		   System.out.print(A[i] + ", ");
+	   }
    }
 }
